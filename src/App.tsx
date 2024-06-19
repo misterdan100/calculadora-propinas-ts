0fbcc549e5477a987019938a5e19e5f73b989a83
misterdan100 <danielmca60@gmail.com>
2024-06-18 21:54:47 -0500

    chore: get data from db and show products as components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
-
+import MenuItem from './components/MenuItem'
+import { menuItems } from "./data/db"
 
 function App() {
 
+
   return (
     <>
-      <div className="">New project</div>
+      <header className="bg-teal-400 py-5">
+        <h1 className="text-center text-4xl font-black">Tips and Consumption Calculator</h1>
+      </header>
+
+      <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2 ">
+        <div className='p-5'>
+          <h2 className='text-4xl font-black'>Menu</h2>
+
+          <div className="space-y-3 mt-10">
+            {menuItems.map( item => (
+              <MenuItem 
+              key={item.id}
+              item={item}
+              />
+            ))}
+          </div>
+        </div>
+
+        <div>
+          <h2>Consumption</h2>
+        </div>
+
+      </main>
     </>
   )
 }
---
+++ src/components/MenuItem.tsx
@@ -0,0 +1,18 @@
+import type { MenuItem } from "../types"
+
+type MenuItemProps = {
+    item: MenuItem
+}
+
+const MenuItem = ({item}: MenuItemProps) => {
+  return (
+    <button
+        className="border-2 border-teal-400 hover:bg-teal-50 w-full p-3 flex justify-between rounded-xl"
+    >
+        <p>{item.name}</p>
+        <p className="font-black">$ {item.price}</p>
+    </button>
+  )
+}
+
+export default MenuItem
